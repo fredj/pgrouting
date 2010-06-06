884d86bdee152804058a4259d2a3e75876108f97
Frederic Junod <frederic.junod@gmail.com>
2010-06-06 11:43:02 +0200

    geoext mappanel

--- pgrouting/static/app.js
+++ pgrouting/static/app.js
@@ -1,6 +1,11 @@
+var mapPanel;
 Ext.onReady(function() {
     var map = new OpenLayers.Map();
     var osm = new OpenLayers.Layer.OSM("Simple OSM Map");
 
     map.addLayer(osm);
+
+//     mapPanel = new GeoExt.MapPanel({
+//         map: map
+//     });
 });
