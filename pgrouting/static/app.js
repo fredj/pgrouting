6e79930e4ab3ca305cda5c83176277e06cdb5858
Frederic Junod <frederic.junod@gmail.com>
2010-06-10 06:28:19 +0200

    use an ext viewport

--- pgrouting/static/app.js
+++ pgrouting/static/app.js
@@ -1,13 +1,28 @@
-var mapPanel;
-
 Ext.onReady(function() {
+    var header = new Ext.Panel({
+        region: 'north',
+        height: 50
+    });
 
-    mapPanel = new GeoExt.MapPanel({
-        renderTo: "map",
+    var map = new GeoExt.MapPanel({
+        region: 'center',
         map: {
              layers: [new OpenLayers.Layer.OSM("Simple OSM Map")]
         },
         center: [245300, 5070600],
         zoom: 12
+    });
+
+    var form = new Ext.form.FormPanel({
+        region: 'south',
+        height: 150
+    });
+
+    var viewport = new Ext.Viewport({
+        layout: 'border',
+        defaults: {
+            frame: false
+        },
+        items: [header, map, form]
     });
 });
